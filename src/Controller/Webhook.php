dd45b4af9bcff4fef47b23689b1778a5429a9464
vdoespiritosanto <vdoespiritosanto@com-company.fr>
2024-06-17 10:56:17 +0200

    Feature/webhooks (#5)
    handle webhooks

--- src/Controller/Webhook.php
+++ src/Controller/Webhook.php
@@ -5,23 +5,28 @@ namespace ComCompany\YousignBundle\Controller;
 use App\Exception\ApplicationException;
 use App\Exception\JsonRequestException;
 use App\Manager\Webhook\Yousign;
-use ComCompany\YousignBundle\YousignV3\WebhookManager;
+use ComCompany\YousignBundle\Service\YousignV3\WebhookManager;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class Webhook
 {
-    /*
-     * @Route("/api/subscription/yousign/webhook", methods={"POST"}, name="subscription.yousign.webhook")
-     */
-    /* public function __invoke(WebhookManager $webhook, Request $request): Response
-     {
-         try {
-             $webhook->handle($request);
-         } catch (ApplicationException $e) {
-             throw new JsonRequestException($e->getMessage(), $e->getCode(), $e, $e->getErrors());
-         }
+    private WebhookManager $manager;
 
-         return Response::create([]);
-     }*/
+    public function __construct(WebhookManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        try {
+            $this->manager->handle($request);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), $e->getCode(), $e, $e->getErrors());
+        }
+
+        return new Response();
+    }
 }
